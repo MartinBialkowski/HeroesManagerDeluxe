e41f4195842c9fb2c7751b81a3da06f2e6d89a43
Martin Białkowski <embe2sc@gmail.com>
2016-10-07 10:48:50 +0200

    MVVM setup

--- HeroesManagerDeluxe/App.xaml.cs
+++ HeroesManagerDeluxe/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using HeroesManagerDeluxe.ViewModel;
 
 namespace HeroesManagerDeluxe
 {
@@ -13,5 +14,24 @@ namespace HeroesManagerDeluxe
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+
+            var viewModel = new MainViewModel();
+
+            EventHandler handler = null;
+            handler = delegate
+            {
+                viewModel.RequestClose -= handler;
+                window.Close();
+            };
+            viewModel.RequestClose += handler;
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
     }
 }
